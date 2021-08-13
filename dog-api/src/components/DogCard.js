a42d03f422e7a858a46c193da64db3c5c240008c
Jon Solari <jon.s.solari@gmail.com>
2021-08-13 08:44:44 -0400

    comment cleanup

--- dog-api/src/components/DogCard.js
+++ dog-api/src/components/DogCard.js
@@ -3,21 +3,6 @@ import axios from 'axios';
 
 export default function DogCard(props){
 
-// const [info, setInfo] = useState({});
-// const [name, setName] = useState();
-
-   
-// useEffect(() => {
-//     axios.get('https://dog.ceo/api/breeds/image/random')
-//         .then(response => {
-//             setInfo(response.data);
-//             //getting the image URL
-//             setName(response.data.message.split('/')[4].split('-').reverse().join(' ').toUpperCase())}
-//             //an attempt to extract the breed name with the 'random' endpoint. the naming convention seems to be a "last-first" one for most of them
-//         )
-//         .catch(err => console.log(err));
-//     }, [])
-
 return (
     <div className="dog-card">
         <p className="text">ß{props.name}</p>
